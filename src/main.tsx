2e184a961c8e834585f62a728601e2a7d236821c
vansiiidotcom <vanshikawadhwaa@gmail.com>
2025-06-29 19:20:21 +0530

    js updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
 import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<div>Portfolio</div>} />
-        <Route path="/contact" element={<div>Contact</div>} />
-        <Route path="/art-gallery" element={<div>Art Gallery</div>} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
-  darkMode: 'class', // Enable dark mode support
   theme: {
     extend: {
       colors: {
-        'vansiii-black': '#101010',
-        'vansiii-white': '#FAFAF9',
-        'vansiii-accent': '#FF5A73',
+        'vansiii-accent': '#FF5733',
       },
     },
   },
